926dc3fcb98480b7993560dbc16c9735f8e7169b
SaidamirRahimtorayev <saidamirxonrahimtorayev@gmail.com>
2024-05-04 15:03:58 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let a = [2, 4, 9, 10, 45, 67, 8, 90];
+  
+console.log(a + ' Boshlanishidagi Array');
+
+for (let i = 0; i < a.length; i++) {
+   if (a[i] % 2 === 0) {
+    console.log(a[i] + ' Juft sonlar');
+   } 
+}
